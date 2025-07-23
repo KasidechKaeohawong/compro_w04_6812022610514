cac6434087d259de96f36f95ab068b1140713855
KasidechKaeohawong <s6812022610514@email.kmutnb.ac.th>
2025-07-23 15:47:58 +0700

    compro_w04_6812022610514

---
+++ coding_w04-01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>  // ใช้สำหรับเรียกฟังก์ชัน printf() จาก standard I/O library
+
+int main() {        // 'int' = Keyword, 'main' = Identifier, '()' = Operator
+
+    printf("Hello,\n");  // 'printf' = Identifier , Comstant "Hello\n"
+    printf("C Tokens!\n");  // 'printf' = Identifier , Comstant "C Tokens!\n"
+
+    return 0;  // 'return' = Keyword, '0' = Constant, ';' = Operator
+}
+/*Token Type	ใช้อยู่ในโค้ด
+Keyword	int, return
+Identifier	main, printf
+Constant	"Hello,\n", "C Tokens!\n", 0
+Operator	(), ;, =   */
---
+++ coding_w04-02.c
@@ -0,0 +1,8 @@
+#include <stdio.h>  //ไม่มี # หน้าคำสั่ง include ต้องเป็น #include <stdio.h>
+int main() {  
+    int a = 10;   //ไม่มี ; หลัง int a = 10
+
+    printf("a = %d", a);  //ไม่มี ; หลัง printf("a = %d", a)
+
+    return 0;  //ไม่มี ; หลัง return 0
+}
